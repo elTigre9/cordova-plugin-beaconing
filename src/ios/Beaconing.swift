bb4028263fb8479f5f7bbf40f6af9135378dfb7e
Admin <ac@Admins-MBP.wi.rr.com>
2019-02-20 11:43:31 -0600

    Useable Updates

--- src/ios/Beaconing.swift
+++ src/ios/Beaconing.swift
@@ -6,8 +6,14 @@ import CoreLocation
 @objc(Beaconing) class Beaconing : CDVPlugin, CLLocationManagerDelegate {
     
     var locationManager = CLLocationManager()
-    var callBackId = String()
     
+    struct delegateIds {
+        var rangeBeaconId: String
+        var monitorBeaconId: String
+        var enteredRegionId: String
+        var leftRegionId: String
+    }
+
     override func pluginInitialize() {
         super.pluginInitialize()
         print("adding delegate")
@@ -16,15 +22,15 @@ import CoreLocation
         
     }
     
-    
-    
-  @objc(beaconDelegate:) // Declare your function name.
-  func beaconDelegate(command: CDVInvokedUrlCommand) { // write the function code.
+    // the listeners get called based on the functions in ionic .ts code
+    // they provide callback ids, which are used to send delegate results to ionic 
+  @objc(rangeBeaconIdListener:) // Declare your function name.
+  func rangeBeaconIdListener(command: CDVInvokedUrlCommand) { // write the function code.
     
     self.commandDelegate.run {
-        print("beaconDelegate", command.callbackId)
+        print("adding ranging beacon listener", command.callbackId)
         // get the callbackid
-        self.callBackId = command.callbackId
+        delegateIds.rangeBeaconId = command.callbackId
         /*
          * Always assume that the plugin will fail.
          * Even if in this example, it can't.
@@ -34,6 +40,54 @@ import CoreLocation
         
         pluginResult?.setKeepCallbackAs(true)
         // Send the function result back to Cordova.
+        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+    }
+  }
+
+  @objc(monitorBeaconIdListener:) // Declare your function name.
+  func monitorBeaconIdListener(command: CDVInvokedUrlCommand) { // write the function code.
+    
+    self.commandDelegate.run {
+        print("adding monitoring beacon listener", command.callbackId)
+        
+        delegateIds.monitorBeaconId = command.callbackId
+
+        let pluginResult = CDVPluginResult (status: CDVCommandStatus_NO_RESULT);
+        
+        pluginResult?.setKeepCallbackAs(true)
+       
+        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+    }
+  }
+
+  @objc(enteredRegionIdListener:) // Declare your function name.
+  func enteredRegionIdListener(command: CDVInvokedUrlCommand) { // write the function code.
+    
+    self.commandDelegate.run {
+        print("adding beacon entered region listener", command.callbackId)
+        
+        delegateIds.enteredRegionId = command.callbackId
+
+        let pluginResult = CDVPluginResult (status: CDVCommandStatus_NO_RESULT);
+        
+        pluginResult?.setKeepCallbackAs(true)
+       
+        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+    }
+  }
+
+  @objc(leftRegionIdListener:) // Declare your function name.
+  func leftRegionIdListener(command: CDVInvokedUrlCommand) { // write the function code.
+    
+    self.commandDelegate.run {
+        print("adding beacon exit region listener", command.callbackId)
+        
+        delegateIds.leftRegionId = command.callbackId
+
+        let pluginResult = CDVPluginResult (status: CDVCommandStatus_NO_RESULT);
+        
+        pluginResult?.setKeepCallbackAs(true)
+       
         self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
     }
   }
@@ -66,30 +120,32 @@ import CoreLocation
         self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
   }
 
-  @objc(authManager: ) // declare function name
-    func authManager(command: CDVInvokedUrlCommand) { // [uuid, major, minor, identifier/name]
-    print("auth manager: ", command.callbackId)
-        var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Authorization and management Failed");
+@objc(monitorBeacons:)
+  func monitorBeacons(command: CDVInvokedUrlCommand) {
+    print("monitorBeacons", command.callbackId)
+    print("beacon array string: ", command.arguments![0])
 
+        // Set the plugin result to fail.
+        var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "The Plugin Failed");
+        
 
-         // Set the plugin result to succeed.
-        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Authorization and management succeeded");
-    
-        // Send the function result back to Cordova.
+        // turn the csv style string to an array
+    let newBeaconList = (command.arguments![0] as AnyObject).components(separatedBy: ",") // or "\n"
+        print("newBeaconList")
+        print(newBeaconList)
+
+        let uuid = UUID(uuidString: newBeaconList[0]) // UUID(uuidString: "426C7565-4368-6172-6D42-6561636F6E73")
+        let major: CLBeaconMajorValue = CLBeaconMajorValue(Int(newBeaconList[1])!) // 3838
+        let minor: CLBeaconMinorValue = CLBeaconMinorValue(Int(newBeaconList[2])!) // 4949
+        let id: String = newBeaconList[3] // "bennyBeacon"
+
+        let region = CLBeaconRegion(proximityUUID: uuid!, major: major, minor: minor, identifier: id)
+
+        self.locationManager.startMonitoring(for: region)
+        // return region as an object
+        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "region set!");
         self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
-    }
-    
-//    func _handleCallSafely(command: CDVInvokedUrlCommand){
-//        var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Authorization and management Failed");
-//
-//
-//        // Set the plugin result to succeed.
-//        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Authorization and management succeeded");
-//
-//        // Send the function result back to Cordova.
-//        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
-//    }
-
+  }
     
     // MARK: Delegate functions
     
@@ -113,24 +169,55 @@ import CoreLocation
         
         pluginResult?.setKeepCallbackAs(true)
         
-        self.commandDelegate.send(pluginResult, callbackId: self.callBackId)
+        self.commandDelegate.send(pluginResult, callbackId: delegateIds.rangeBeaconId)
     }
     
     @objc func locationManager(_ manager: CLLocationManager, didStartMonitoringFor region: CLRegion) {
         print("monitoring beacon")
         self.commandDelegate.run {
-            guard (region.identifier) != "" else { print("couldn't find a ranged beacon"); return}
+            guard (region.identifier) != "" else { print("couldn't find a monitored beacon"); return}
             
             print("within beacon range!", region)
             print(region.identifier)
             
-            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: region.identifier);
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: region.identifier)
             
             pluginResult?.setKeepCallbackAs(true)
             
-            self.commandDelegate.send(pluginResult, callbackId: self.callBackId)
+            self.commandDelegate.send(pluginResult, callbackId: delegateIds.monitorBeaconId)
         }
         
     }
 
+    @objc func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print("checking if entered region")
+        self.commandDelegate.run {
+            guard (region.identifier) != "" else { print("couldn't find a beacon"); return}
+            
+            print("entered beacon region!", region)
+            print(region.identifier)
+            
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: region.identifier)
+            
+            pluginResult?.setKeepCallbackAs(true)
+            
+            self.commandDelegate.send(pluginResult, callbackId: delegateIds.enteredRegionId)
+        }
+    }
+
+    @objc func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        print("checking if exited region")
+        self.commandDelegate.run {
+            guard (region.identifier) != "" else { print("couldn't find a beacon"); return}
+            
+            print("exited beacon region!", region)
+            print(region.identifier)
+            
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: region.identifier)
+            
+            pluginResult?.setKeepCallbackAs(true)
+            
+            self.commandDelegate.send(pluginResult, callbackId: delegateIds.leftRegionId)
+        }
+    }
 }
--- www/Beaconing.js
+++ www/Beaconing.js
@@ -5,12 +5,24 @@ var PLUGIN_NAME = "Beaconing"; // This is just for code completion uses.
 var Beaconing = function () {}; // This just makes it easier for us to export all of the functions at once.
 // All of your plugin functions go below this. 
 // Note: We are not passing any options in the [] block for this, so make sure you include the empty [] block.
-Beaconing.beaconDelegate = function (onSuccess, onError) {
-    exec(onSuccess, onError, PLUGIN_NAME, "beaconDelegate", []);
+Beaconing.rangeBeaconIdListener = function (onSuccess, onError) {
+    exec(onSuccess, onError, PLUGIN_NAME, "rangeBeaconIdListener", []);
 };
 
-Beaconing.authManager = function (onSuccess, onError) {
-    exec(onSuccess, onError, PLUGIN_NAME, "authManager", []);
+Beaconing.monitorBeaconIdListener = function (onSuccess, onError) {
+    exec(onSuccess, onError, PLUGIN_NAME, "monitorBeaconIdListener", []);
+};
+
+Beaconing.enteredRegionIdListener = function (onSuccess, onError) {
+    exec(onSuccess, onError, PLUGIN_NAME, "enteredRegionIdListener", []);
+};
+
+Beaconing.leftRegionIdListener = function (onSuccess, onError) {
+    exec(onSuccess, onError, PLUGIN_NAME, "leftRegionIdListener", []);
+};
+
+Beaconing.rangeBeacons = function (options, onSuccess, onError) {
+    exec(onSuccess, onError, PLUGIN_NAME, "rangeBeacons", [options]);
 };
 
 Beaconing.rangeBeacons = function (options, onSuccess, onError) {
